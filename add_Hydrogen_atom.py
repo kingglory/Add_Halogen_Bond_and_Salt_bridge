945d81b2f658c5a5f1a6f6d74968467b6f5cfc72
wangwensong <wangwensog@shu.edu.cn>
2018-10-07 20:19:26 +0800

    come back

--- add_Hydrogen_atom.py
+++ add_Hydrogen_atom.py
@@ -2,13 +2,14 @@
 #2018.10.7
 #define a function to add hydrogen atoms for pdb file in that to find Hydrogen bond
 import phenix
+import os
 
+def add_hydrogen_atoms():
 
-def add_hydrogen_atoms(pdb_file, pdb_add_H_file=None):
-    phenix.reduce (pdb_file, pdb_add_H_file)
-    return pdb_add_H_file
+   os.system("phenix.reduce %s > %s " %('1b20.pdb' ,'1b20'+'h'+'.pdb'))
 
 
 #test
 pdb_file = "1b20.pdb"
-add_hydrogen_atoms(pdb_file)
+
+add_hydrogen_atoms()
