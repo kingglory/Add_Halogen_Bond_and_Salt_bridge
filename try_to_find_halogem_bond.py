6c87f388f59a8e789e4701c685acab8da0f15234
wangwensong <wangwensog@shu.edu.cn>
2018-10-08 11:31:59 +0800

    refresh

--- try_to_find_halogem_bond.py
+++ try_to_find_halogem_bond.py
@@ -2,7 +2,7 @@
 #2018.10.7
 
 from bond_distance import distance
-from bond_angle import angle
+import bond_angle
 from change_pdb_into_dataframe import change_the_pdb_file_into_dataframe
 
 
@@ -21,15 +21,23 @@ def try_to_find_halogen_bond(pdb_file):
         i = i+1
 
     k = 0
+    h = 0
     for t in range(len(halogen_atom)):
         for k in Halogen_bond_pdb_file_atom_label:
             atom2 = Halogen_bond_pdb_file_atom_label.loc[['X','Y','Z'],k].astype('float16')
+            k = k+1
             atom2 = list(atom2.values)
             bond_lengh = distance(halogen_atom[t],atom2)
-            if 1<=bond_lengh<=3:
-                print Halogen_bond_pdb_file_atom_label.loc[:,k]
-        k = k + 1
-
+            if 1<=bond_lengh<=5:
+                for h in Halogen_bond_pdb_file_atom_label:
+                    atom3 = Halogen_bond_pdb_file_atom_label.loc[['X','Y','Z'],k].astype('float16')
+                    atom3 = list(atom3.values)
+                    bond_lengh = distance(atom2,atom3)
+                    if 1<bond_lengh<5:
+                         from bond_angle import angle
+                         bond_angle = angle(halogen_atom[t],atom2,atom3)
+                         if 90<bond_angle<180:
+                             print 'find halogen bond !'
 
 
 
