98b9d3c843650eeaafc965b937ccb84199d11da1
Pavel <pafonine@lbl.gov>
2018-10-09 15:54:42 +0800

    New PDB, plus some more progress!

--- run.py
+++ run.py
@@ -14,37 +14,45 @@ def distance(s1, s2):
 
 
 def get_halogen_bond_pairs(hierarchy, vdwr):
-    halogens = ["CL", "BR", "I", "F"]
-    halogen_bond_pairs_atom = ["O","N"]
-    #print dir(mmtbx.model.atom)
-    for atom_1 in hierarchy.atoms():
-        e1 = atom_1.element.strip().upper()
-        if (e1 in halogens):
-            #print e1, vdwr[e1]
-            for atom_2 in hierarchy.atoms():
-              e2 = atom_2.element.strip().upper()
-              if (e2 in halogen_bond_pairs_atom):
-                # d = distance(atom_1.xyz, atom_2.xyz)
-                d = atom_1.distance(atom_2)
-                sum_vdwr = vdwr[e1] + vdwr[e2]
-                sum_vdwr_min = sum_vdwr*0.6
-                
-                if (sum_vdwr_min<d<sum_vdwr):
-                    if (atom_1.chain!=atom_2.chain):
-                    # print sum_vdwr_min,sum_vdwr
-                      print (d,e1,e2,atom_1,atom_2,atom_1.chain,atom_1.id_str,atom_2.chain,atom_2.id_str)
-                # a = atom_1.angle(atom_1=b, atom_3=b, deg=True)
-                #print d
+  halogens = ["CL", "BR", "I", "F"]
+  halogen_bond_pairs_atom = ["O","N","S"]
+  #print dir(mmtbx.model.atom)
+  for atom_1 in hierarchy.atoms():
+    e1 = atom_1.element.strip().upper()
+    if (e1 in halogens):
+      #print e1, vdwr[e1]
+      for atom_2 in hierarchy.atoms():
+        e2 = atom_2.element.strip().upper()
+        if (e2 in halogen_bond_pairs_atom):
+          # d = distance(atom_1.xyz, atom_2.xyz)
+          d = atom_1.distance(atom_2)
+          
+          ai1 = atom_1.id_str().replace("pdb=","")
+          ai2 = atom_2.id_str().replace("pdb=","")
+          print ai1, ai2
+          
+          
+          sum_vdwr = vdwr[e1] + vdwr[e2]
+          sum_vdwr_min = sum_vdwr*0.6
+          if (sum_vdwr_min<d<sum_vdwr):
+            if (atom_1.chain!=atom_2.chain):
+            # print sum_vdwr_min,sum_vdwr
+              ai1 = atom_1.id_str().replace("pdb=","")
+              ai2 = atom_2.id_str().replace("pdb=","")
+              #print (d,e1,e2,ai1,ai2)
+          # a = atom_1.angle(atom_1=b, atom_3=b, deg=True)
+          #print d
 
 
 if __name__ == '__main__':
-    pdb_file = "2ito.pdb"
+    pdb_file = "5v7d.pdb"
     pdb_inp = iotbx.pdb.input(file_name=pdb_file)
     model = mmtbx.model.manager(
         model_input=pdb_inp,
         process_input=True,
         log=null_out())
     vdwr = model.get_vdw_radii()
+    print vdwr.keys()    
     get_halogen_bond_pairs(
         hierarchy=model.get_hierarchy(),
         vdwr=vdwr)
