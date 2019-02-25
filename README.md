aef95bb1d3459cb01ec4852a5413f52888c48b85
kingglory <312775704@qq.com>
2019-02-25 17:16:50 +0800

    refresh name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Find_Halogen_Bond_and_Salt_bridge
+# Find_noncovalent_interactions_in_protein
 The project is to write a CCTBX based Python program that would take a PDB file with a protein structure and find all halogen bonds and salt bridges in the structure (meaning finding all pairs of amino-acid residues participating in said interactions). 
 
 
--- hydrogen_bonds_test.py
+++ hydrogen_bonds_test.py
@@ -24,10 +24,7 @@ def get_model(pdb_file_name, cif_file_name):
   return model
 
 def exercise():
-  files = [#["1jvuh.pdb","1jvuh.ligands.cif"],
-           #["2x7gh.pdb","2x7gh.ligands.cif"],
-           #["3alph.pdb","3alph.ligands.cif"],
-           #["4x7nh.pdb","4x7nh.ligands.cif"],
+  files = [
            ["1aieh.pdb",None],
            ["1kych.pdb",None],
            ["3q8jh.pdb",None],
