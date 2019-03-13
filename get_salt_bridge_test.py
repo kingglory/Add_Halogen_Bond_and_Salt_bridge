20ee470857ee823e6955b04c5f2864c46a7e83cd
Wangwensong <312775704@qq.com>
2019-03-14 06:40:33 +0800

    Update get_salt_bridge_test.py
    a

--- get_salt_bridge_test.py
+++ get_salt_bridge_test.py
@@ -27,13 +27,31 @@ def exercise():
            ["3alph.pdb","3alph.ligands.cif"],
            ["4x7nh.pdb","4x7nh.ligands.cif"]
            ]
+  salt_bridge_sites = [
+    ('pdb=" HE  ARG B  10 "', 'pdb=" NE  ARG B  10 "', 'pdb=" OE2 GLU B   2 "'),
+    ('pdb=" HZ2 LYS A 218 "', 'pdb=" NZ  LYS A 218 "', 'pdb=" OD2 ASP A 150 "'),
+    ('pdb="HH22 ARG A  96 "', 'pdb=" NH2 ARG A  96 "', 'pdb=" OD2 ASP A 118 "'),
+    ('pdb="HH11 ARG B 101 "', 'pdb=" NH1 ARG B 101 "', 'pdb=" OD2 ASP B 106 "'),
+    ('pdb="HH22 ARG B  96 "', 'pdb=" NH2 ARG B  96 "', 'pdb=" OD1 ASP B 118 "'),
+    ('pdb="HH21 ARG B  96 "', 'pdb=" NH2 ARG B  96 "', 'pdb=" OD2 ASP B 118 "'),
+    ('pdb=" HZ1 LYS B 178 "', 'pdb=" NZ  LYS B 178 "', 'pdb=" OE2 GLU B 119 "'),
+    ('pdb=" HZ3 LYS B 166 "', 'pdb=" NZ  LYS B 166 "', 'pdb=" OE2 GLU B 192 "'),
+    ('pdb="HH21 ARG B 217 "', 'pdb=" NH2 ARG B 217 "', 'pdb=" OD1 ASP B 272 "'),
+    ('pdb=" HZ1 LYS A 893 "', 'pdb=" NZ  LYS A 893 "', 'pdb=" OE1 GLU A 609 "'),
+    ('pdb=" HE  ARG A 638 "', 'pdb=" NE  ARG A 638 "', 'pdb=" OE1 GLU A 634 "'),
+    ('pdb="HH22 ARG A1065 "', 'pdb=" NH2 ARG A1065 "', 'pdb=" OE2 GLU A 994 "'),
+    ('pdb=" HE  ARG A 911 "', 'pdb=" NE  ARG A 911 "', 'pdb=" OE2 GLU A1050 "')
+  ]
+                       
   for (pdb_file_name, cif_file_name) in files:
     print (pdb_file_name, "-"*50)
     model = get_model(pdb_file_name=pdb_file_name, cif_file_name=cif_file_name)
     result1 = find_salt_bridge(model=model)
     for r in result1:
       print ("%s"% r.atom_1.id_str(), r.atom_2.id_str(),r.atom_3.id_str())
-      # assert  XXX
+      assert  (r.atom_1.id_str(), r.atom_2.id_str(),
+               r.atom_3.id_str()) in  salt_bridge_sites
+      
 
 
 if __name__ == '__main__':
