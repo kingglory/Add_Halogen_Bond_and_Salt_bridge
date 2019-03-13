159ff147758ae2139580ea8e9895a3f5b19ccefd
Wangwensong <312775704@qq.com>
2019-03-14 07:15:14 +0800

    Update run.py
    a

--- run.py
+++ run.py
@@ -220,7 +220,7 @@ def find_salt_bridge(model, min = 1.7, max = 2.2, eps1 = 0.15, shutoff = 4 ):
       if(n2 not in vdwr.keys()): continue
       sum_vdwr = vdwr[n3] + vdwr[n2]
       d_32 = a3.distance(a2)
-      if(d_32 > shutoff): continue
+      if(d_32 > shutoff - eps1): continue
       d_13 = a1.distance(a3)
       if (min-eps1 < d_13 < max+eps1):
         angle_312 = (a1.angle(a2, a3, deg=True))
