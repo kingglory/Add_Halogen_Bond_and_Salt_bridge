7ccf5e59b757190d60da5cc548c100e37091d4a5
Wangwensong <312775704@qq.com>
2019-03-14 00:41:56 +0800

    Update hydrogen_bonds_test.py
    f

--- hydrogen_bonds_test.py
+++ hydrogen_bonds_test.py
@@ -33,18 +33,20 @@ def exercise():
            ["2ona_complete_occH0_qm_refined.pdb",None]
            ]
   Hydrogen_atom_pairs = [
-    ('pdb=" HE AARG A 342 "', 'pdb=" OE2 GLU A 346 "'),
-    ('pdb="HH21AARG A 342 "', 'pdb=" OE2 GLU A 346 "'),
-    ('pdb=" H   ARG A   4 "', 'pdb=" O1  SIN A   0 "'),
+    ('pdb=" HD3BARG A 342 "', 'pdb=" OE2 GLU A 346 "'),
+    ('pdb=" HG3 LYS A 351 "', 'pdb=" OXT GLY A 356 "'),
     ('pdb="HH22 ARG A  13 "', 'pdb=" OE1 GLU A   9 "'),
     ('pdb="HH21 ARG A  15 "', 'pdb=" O3  SO4 A 101 "'),
-    ('pdb="HD21 ASN A  11 "', 'pdb=" OD1 ASP A  31 "'),
-    ('pdb=" H   GLN A  13 "', 'pdb=" OD1 ASN A  11 "'),
+    ('pdb=" H   ARG A   4 "', 'pdb=" O1  SIN A   0 "'),
+    ('pdb=" H   PHE A   5 "', 'pdb=" OE1 GLU A   8 "'),
     ('pdb=" H   PHE A  14 "', 'pdb=" OD1 ASN A  11 "'),
+    ('pdb="HD21 ASN A  11 "', 'pdb=" OD1 ASP A  31 "'),
     ('pdb=" H   SER A 703 "', 'pdb=" OE2 GLU A 706 "'),
     ('pdb=" HD2 ARG A 712 "', 'pdb=" OH  TYR A 708 "'),
+    ('pdb="HE21 GLN B   5 "', 'pdb=" OD1 ASP A  17 "'),
     ('pdb=" H   TYR A  46 "', 'pdb=" OD2 ASP A  22 "'),
-    ('pdb=" H2  HOH B   7 "', 'pdb=" OXT VAL B   6 "')
+    ('pdb=" HB3 SER A  37 "', 'pdb=" OE2 GLU A  39 "'),
+    ('pdb=" H2  HOH B   7 "', 'pdb=" OXT VAL B   6 "')   
   ]
   for (pdb_file_name, cif_file_name) in files:
     print (pdb_file_name, "-"*50)
