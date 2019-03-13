c128e47b6ab847a37b71a1991539b0ad45cffd0a
Wangwensong <312775704@qq.com>
2019-03-14 07:25:52 +0800

    Update get_salt_bridge_test.py
    kaixin

--- get_salt_bridge_test.py
+++ get_salt_bridge_test.py
@@ -28,18 +28,36 @@ def exercise():
            ["4x7nh.pdb","4x7nh.ligands.cif"]
            ]
   salt_bridge_sites = [
+    ('pdb="HH21 ARG A  10 "', 'pdb=" NH2 ARG A  10 "', 'pdb=" OE1 GLU A   2 "'),
+    ('pdb=" HE  ARG A  10 "', 'pdb=" NE  ARG A  10 "', 'pdb=" OE2 GLU A   2 "'),
+    ('pdb=" HE  ARG A  85 "', 'pdb=" NE  ARG A  85 "', 'pdb=" OD1 ASP A  83 "'),
+    ('pdb="HH21 ARG B  10 "', 'pdb=" NH2 ARG B  10 "', 'pdb=" OE1 GLU B   2 "'),
     ('pdb=" HE  ARG B  10 "', 'pdb=" NE  ARG B  10 "', 'pdb=" OE2 GLU B   2 "'),
+    ('pdb=" HE  ARG A  91 "', 'pdb=" NE  ARG A  91 "', 'pdb=" OD1 ASP A 111 "'),
+    ('pdb="HH21 ARG A  91 "', 'pdb=" NH2 ARG A  91 "', 'pdb=" OD2 ASP A 111 "'),
+    ('pdb=" HZ3 LYS A 121 "', 'pdb=" NZ  LYS A 121 "', 'pdb=" OE1 GLU A 136 "'),
+    ('pdb=" HZ3 LYS A 218 "', 'pdb=" NZ  LYS A 218 "', 'pdb=" OD2 ASP A 147 "'),
     ('pdb=" HZ2 LYS A 218 "', 'pdb=" NZ  LYS A 218 "', 'pdb=" OD2 ASP A 150 "'),
+    ('pdb="HH22 ARG A 685 "', 'pdb=" NH2 ARG A 685 "', 'pdb=" OE1 GLU A 565 "'),
+    ('pdb="HH12 ARG A 685 "', 'pdb=" NH1 ARG A 685 "', 'pdb=" OE2 GLU A 565 "'),
+    ('pdb="HH12 ARG A 559 "', 'pdb=" NH1 ARG A 559 "', 'pdb=" OD1 ASP A 606 "'),
     ('pdb="HH22 ARG A  96 "', 'pdb=" NH2 ARG A  96 "', 'pdb=" OD2 ASP A 118 "'),
+    ('pdb=" HZ2 LYS A 236 "', 'pdb=" NZ  LYS A 236 "', 'pdb=" OE1 GLU A 186 "'),
+    ('pdb=" HZ2 LYS A 166 "', 'pdb=" NZ  LYS A 166 "', 'pdb=" OE2 GLU A 192 "'),
+    ('pdb="HH12 ARG A 212 "', 'pdb=" NH1 ARG A 212 "', 'pdb=" OE2 GLU A 194 "'),
     ('pdb="HH11 ARG B 101 "', 'pdb=" NH1 ARG B 101 "', 'pdb=" OD2 ASP B 106 "'),
     ('pdb="HH22 ARG B  96 "', 'pdb=" NH2 ARG B  96 "', 'pdb=" OD1 ASP B 118 "'),
     ('pdb="HH21 ARG B  96 "', 'pdb=" NH2 ARG B  96 "', 'pdb=" OD2 ASP B 118 "'),
     ('pdb=" HZ1 LYS B 178 "', 'pdb=" NZ  LYS B 178 "', 'pdb=" OE2 GLU B 119 "'),
+    ('pdb=" HZ2 LYS B 236 "', 'pdb=" NZ  LYS B 236 "', 'pdb=" OE2 GLU B 186 "'),
     ('pdb=" HZ3 LYS B 166 "', 'pdb=" NZ  LYS B 166 "', 'pdb=" OE2 GLU B 192 "'),
     ('pdb="HH21 ARG B 217 "', 'pdb=" NH2 ARG B 217 "', 'pdb=" OD1 ASP B 272 "'),
     ('pdb=" HZ1 LYS A 893 "', 'pdb=" NZ  LYS A 893 "', 'pdb=" OE1 GLU A 609 "'),
     ('pdb=" HE  ARG A 638 "', 'pdb=" NE  ARG A 638 "', 'pdb=" OE1 GLU A 634 "'),
+    ('pdb="HH21 ARG A 638 "', 'pdb=" NH2 ARG A 638 "', 'pdb=" OE2 GLU A 634 "'),
+    ('pdb="HH12 ARG A1065 "', 'pdb=" NH1 ARG A1065 "', 'pdb=" OE1 GLU A 994 "'),
     ('pdb="HH22 ARG A1065 "', 'pdb=" NH2 ARG A1065 "', 'pdb=" OE2 GLU A 994 "'),
+    ('pdb=" HE  ARG A1027 "', 'pdb=" NE  ARG A1027 "', 'pdb=" OE1 GLU A1016 "'),
     ('pdb=" HE  ARG A 911 "', 'pdb=" NE  ARG A 911 "', 'pdb=" OE2 GLU A1050 "')
   ]
                        
@@ -49,10 +67,10 @@ def exercise():
     result1 = find_salt_bridge(model=model)
     for r in result1:
       print ("%s"% r.atom_1.id_str(), r.atom_2.id_str(),r.atom_3.id_str())
-      """assert  (r.atom_1.id_str(), r.atom_2.id_str(),
+      assert  (r.atom_1.id_str(), r.atom_2.id_str(),
                r.atom_3.id_str()) in  salt_bridge_sites
       
-       """
+       
 
 if __name__ == '__main__':
     t0 = time.time()
