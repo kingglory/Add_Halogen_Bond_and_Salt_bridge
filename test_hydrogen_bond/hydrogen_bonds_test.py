052c863d477a27cf7ed9227ba4342f3eceade4e2
kingglory <312775704@qq.com>
2019-05-04 16:05:34 +0800

    clear beautiful

--- test_hydrogen_bond/hydrogen_bonds_test.py
+++ test_hydrogen_bond/hydrogen_bonds_test.py
@@ -27,16 +27,16 @@ def get_model(pdb_file_name, cif_file_name):
   return model
 
 def exercise():
-  files = [
-           ["1kych.pdb",None],
-           ["1kych1.pdb",None],
-           ["1kych2.pdb",None],
-           ["1kych3.pdb",None],
-           ["1kyc.pdb", None],
-           ["3q8jh1.pdb",None],
-           ["3q8jh2.pdb",None],
-           ["3q8jh.pdb", None],
-           ["6iip.pdb",None],
+  files = [["m-helix.pdb",None]
+           #["1kych.pdb",None],
+           #["1kych1.pdb",None],
+           #["1kych2.pdb",None],
+           #["1kych3.pdb",None],
+           #["1kyc.pdb", None],
+           #["3q8jh1.pdb",None],
+           #["3q8jh2.pdb",None],
+           #["3q8jh.pdb", None],
+           #["6iip.pdb",None],
            #["6iiph.pdb",None]
 
           ]
@@ -85,7 +85,7 @@ def exercise():
     model = get_model(pdb_file_name=pdb_file_name,
                       cif_file_name=cif_file_name)
     get_h_bonds = get_hydrogen_bonds(model=model)
-    get_h_bonds.write_restrains_file(pdb_file_name=pdb_file_name)
+    get_h_bonds.write_restrains_file(pdb_file_name=pdb_file_name,use_defaul_parameters=False)
     results = get_h_bonds.get_hydrogen_bonds_pairs()
     
     for r in results:
