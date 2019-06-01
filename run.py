16910df16ffe69291acfd027db2a37b96738e4ba
kingglory <312775704@qq.com>
2019-06-01 17:18:10 +0800

    had try best , why not try again?!!!

--- run.py
+++ run.py
@@ -359,9 +359,9 @@ class get_hydrogen_bonds(object):
              )
           if (result in results): continue
           if (result is not None): results.append(result)
-
     # just keep the more possiable situation
     # for O(accepter) atom
+    results = list(set(results))
     for i, ri in enumerate(results):
       for j, rj in enumerate(results):
         if (j <= i): continue
